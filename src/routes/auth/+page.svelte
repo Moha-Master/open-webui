402bb00cba5930e333ebcf0d7d95d24f66918b33
Moha-Master <hongkongreporter@outlook.com>
2025-03-22 13:54:01 +0800

    消除对Email的强制要求

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -273,7 +273,7 @@
 											<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Email')}</div>
 											<input
 												bind:value={email}
-												type="email"
+												type="text"
 												class="my-0.5 w-full text-sm outline-hidden bg-transparent"
 												autocomplete="email"
 												name="email"
