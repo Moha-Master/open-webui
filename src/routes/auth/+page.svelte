60cfd7581463efa7e48719a05f2460bb43e7ef9c
Moha-Master <hongkongreporter@outlook.com>
2025-08-22 12:21:08 +0800

    本地化修改

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -319,7 +319,7 @@
 												>
 												<input
 													bind:value={email}
-													type="email"
+													type="text"
 													id="email"
 													class="my-0.5 w-full text-sm outline-hidden bg-transparent placeholder:text-gray-300 dark:placeholder:text-gray-600"
 													autocomplete="email"
