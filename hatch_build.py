fb4c2f0ad8b84900a0ce521dea263e6f3cfeca9c
Moha-Master <hongkongreporter@outlook.com>
2026-04-30 16:54:52 +0800

    调整构建配置

--- hatch_build.py
+++ hatch_build.py
@@ -10,12 +10,16 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         super().initialize(version, build_data)
+        skip_build = os.getenv("SKIP_BUILD", "").strip().lower() in {"1", "true"}
+        if skip_build:
+            stderr.write("### Skipping frontend build\n")
+            return
         stderr.write('>>> Building Open Webui frontend\n')
         npm = shutil.which('npm')
         if npm is None:
             raise RuntimeError('NodeJS `npm` is required for building Open Webui but it was not found')
         stderr.write('### npm install\n')
-        subprocess.run([npm, 'install', '--force'], check=True)  # noqa: S603
+        subprocess.run([npm, 'install', '--force', '--verbose'], check=True)  # noqa: S603
         stderr.write('\n### npm run build\n')
         os.environ['APP_BUILD_HASH'] = version
-        subprocess.run([npm, 'run', 'build'], check=True)  # noqa: S603
+        subprocess.run([npm, 'run', 'build', '--verbose'], check=True)  # noqa: S603
