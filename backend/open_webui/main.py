038a72cdabf0d301d0681d1ce973a3d1c56275c6
Moha-Master <hongkongreporter@outlook.com>
2026-07-27 21:55:00 +0800

    修正PWA图标

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -2863,15 +2863,21 @@ async def get_manifest_json():
                 # Do not alter, remove, obscure, or replace it except as LICENSE permits:
                 # https://docs.openwebui.com/license.
                 {
-                    'src': '/static/logo.png',
+                    'src': '/static/web-app-manifest-192x192.png',
                     'type': 'image/png',
-                    'sizes': '500x500',
+                    'sizes': '192x192',
+                    'purpose': 'maskable',
+                },
+                {
+                    'src': '/static/web-app-manifest-512x512.png',
+                    'type': 'image/png',
+                    'sizes': '512x512',
                     'purpose': 'any',
                 },
                 {
-                    'src': '/static/logo.png',
+                    'src': '/static/web-app-manifest-512x512.png',
                     'type': 'image/png',
-                    'sizes': '500x500',
+                    'sizes': '512x512',
                     'purpose': 'maskable',
                 },
             ],
