c9b1beecbce2a3968be98bf45b122e47a70d46e5
Moha-Master <hongkongreporter@outlook.com>
2026-08-02 23:28:03 +0800

    限制前端展示的图片大小

--- src/lib/components/chat/Messages/UserMessage.svelte
+++ src/lib/components/chat/Messages/UserMessage.svelte
@@ -184,7 +184,7 @@
 									: `${WEBUI_API_BASE_URL}/files/${file.url}${file?.content_type ? '/content' : ''}`}
 							<div class={($settings?.chatBubble ?? true) ? 'self-end' : ''}>
 								{#if file.type === 'image' || (file?.content_type ?? '').startsWith('image/')}
-									<Image src={fileUrl} imageClassName=" max-h-96 rounded-lg" />
+									<Image src={fileUrl} imageClassName=" max-w-[25vw] max-h-[25vh] rounded-lg" />
 								{:else}
 									<FileItem
 										item={file}
